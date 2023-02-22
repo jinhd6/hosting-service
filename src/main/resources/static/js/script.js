d7455ed88f5b88fc0c369edd5f7446c4b300d531
jinhd6 <jinhd6@gmail.com>
2023-02-22 11:19:51 +0900

    Improve flatpickr settings

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,15 +4,26 @@ const main = function() {
     let fpStart = flatpickr("#connectDate", {
         enableTime: true,
         dateFormat: "Z",
-        maxDate: "today",
+        minDate: "today",
+        minTime: new Date(),
+        defaultHour: new Date().getHours(),
+        defaultMinute: new Date().getMinutes(),
         onChange: function(selectedDates, dateStr, instance) {
             fpEnd.set("minDate", dateStr);
+            const now = new Date()
+            if (selectedDates[0].toDateString() === now.toDateString()) {
+                fpStart.set("minTime", now);
+            } else {
+                fpStart.set("minTime", "00:00");
+            }
         }
     });
     let fpEnd = flatpickr("#disconnectDate", {
         enableTime: true,
         dateFormat: "Z",
-        maxDate: "today",
+        minDate: "today",
+        defaultHour: new Date().getHours(),
+        defaultMinute: new Date().getMinutes(),
         onChange: function(selectedDates, dateStr, instance) {
             fpStart.set("maxDate", dateStr);
         }
